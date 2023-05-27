8c392f7d471f2e8dced3b9620a8c0d648e6c9921
Daniel Velez <85912969+vlz0@users.noreply.github.com>
2023-05-26 19:58:06 -0500

    mi primer commit

--- app2/views.py
+++ app2/views.py
@@ -1,19 +1,5 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 from django.shortcuts import render
 
 # Create your views here.
 def app2_vista(request): 
-=======
-from django.shortcuts import render
-
-# Create your views here.
-def app2_vista(request): 
->>>>>>> 22ccbe2 (se avanza en comidas)
-=======
-from django.shortcuts import render
-
-# Create your views here.
-def app2_vista(request): 
->>>>>>> 651dabb795813a6afaad9cb5ddf0a630dfa85e34
     return render(request,'home.html')
